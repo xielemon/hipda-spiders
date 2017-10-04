6f1ae330f9abed92ff7edd6cc9fbe7ed38f547e7
mingfengxie <foo@qq.ccom>
2017-10-04 17:25:39 +0800

    add mysql

--- hipdaspy/hipdaspy/pipelines.py
+++ hipdaspy/hipdaspy/pipelines.py
@@ -6,16 +6,29 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import logging
 from items import *
+import mysql.connector
 
 class HipdaspyPipeline(object):
 
     cnt=0
     logger=0
 
+    config = {
+        'host': '127.0.0.1',
+        'user': 'root',
+        'password': 'root',
+        'port': 3306,
+        'database': 'hipda',
+        'charset': 'utf8'
+    }
+
 
     def __init__(self):
         super(HipdaspyPipeline, self).__init__()
         self.logger=logging.getLogger(type(self).__name__)
+        mysql.connector.connect(**self.config)
+
+
 
     def process_item(self, item, spider):
 
