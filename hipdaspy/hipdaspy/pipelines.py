b83d4e41e62a499176474b0f0717e9d054027ab2
mingfengxie <foo@qq.ccom>
2017-10-06 21:35:34 +0800

    add postlist mysql operation

--- hipdaspy/hipdaspy/pipelines.py
+++ hipdaspy/hipdaspy/pipelines.py
@@ -21,12 +21,14 @@ class HipdaspyPipeline(object):
         'database': 'hipda',
         'charset': 'utf8'
     }
+    conn=0
 
 
     def __init__(self):
         super(HipdaspyPipeline, self).__init__()
         self.logger=logging.getLogger(type(self).__name__)
-        mysql.connector.connect(**self.config)
+        self.conn=mysql.connector.connect(**self.config)
+
 
 
 
@@ -34,6 +36,16 @@ class HipdaspyPipeline(object):
 
         if isinstance(item,HipdaspyItem):
             print item['tid']+":"+item['title']
+            try:
+                cur=self.conn.cursor()
+                sql="insert into post_list (title,author,postTime,link,tid) VALUES (%s,%s,%s,%s,%s)"
+                map=(item['title'],item['author'],item['postTime'],item['link'],item['tid'])
+                cur.execute(sql,map)
+                #auto commit is off
+                self.conn.commit()
+            except mysql.connector.Error as e:
+                print('insert datas error!{}'.format(e))
+
         elif isinstance(item,replyItem):
             print "reply:"+item['tid']
 
